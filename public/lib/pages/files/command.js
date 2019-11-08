2d25f2a5c4654853e70fc3cac6c6b52f56cf07cd
yustarandomname <abeldebruijn@hotmail.com>
2019-11-08 17:25:13 +0100

    added emoji

--- public/lib/pages/files/command.js
+++ public/lib/pages/files/command.js
@@ -1,70 +1,67 @@
 let fileCommands = {
-	saveSource : "users/"+"tf7MLQVgQUg6sHcMq995tKcRVlv1"+"/files",
-	saveFormat : {
-		blocks : 0,
-		fileurl : 'notes',
-		folderid : 0,
-		title : 'untitled',
-		type : 'note',
-		updateKey : 0
+	saveSource: 'users/' + 'tf7MLQVgQUg6sHcMq995tKcRVlv1' + '/files',
+	saveFormat: {
+		blocks: 0,
+		fileurl: 'notes',
+		folderid: 0,
+		title: 'untitled',
+		type: 'note',
+		updateKey: 0
 	},
-	function : {
-		hint : "Open app",
-		icon : "openApp",
-		execute : () => openApp('file')
+	function: {
+		hint: 'Open app',
+		icon: '⤅',
+		execute: () => openApp('file')
 	},
 	//edit : { func : () => console.log('to be implemented')},
 
-	create : {
-		function : {
-			hint : "Inline form",
-			icon : "inlineForm",
-			execute : () => {
-					let content = [
-							{type:'input', placeholder: 'Title'},
-							{type:'input', placeholder: 'Type'},
-					];
-					createInlineForm('Create note',content,{Title:'title',Type:'type'});
+	create: {
+		function: {
+			hint: 'Inline form',
+			icon: '📄',
+			execute: () => {
+				let content = [ { type: 'input', placeholder: 'Title' }, { type: 'input', placeholder: 'Type' } ];
+				createInlineForm('Create note', content, { Title: 'title', Type: 'type' });
 			}
 		},
-		note : {
-			function : {
-				hint : "Inline form",
-				icon : "inlineForm",
-				execute : () => {
+		note: {
+			function: {
+				hint: 'Inline form',
+				icon: '📄',
+				execute: () => {
 					let content = [
-						{type:'input', placeholder: 'Title'},
-						{type:'input', placeholder: 'Type', value: 'note'},
+						{ type: 'input', placeholder: 'Title' },
+						{ type: 'input', placeholder: 'Type', value: 'note' }
 					];
-					createInlineForm('Create note',content,{Title:'title',Type:'type'});
+					createInlineForm('Create note', content, { Title: 'title', Type: 'type' });
 				}
 			}
 		},
-		presentation : { 
-			function : {
-				hint : "Inline form",
-				icon : "inlineForm",
-				execute : () => {
+		presentation: {
+			function: {
+				hint: 'Inline form',
+				icon: 'inlineForm',
+				execute: () => {
 					let content = [
-						{type:'input', placeholder: 'Title'},
-						{type:'input', placeholder: 'Type', value: 'presentation'},
+						{ type: 'input', placeholder: 'Title' },
+						{ type: 'input', placeholder: 'Type', value: 'presentation' }
 					];
-					createInlineForm('Create presentation',content,{Title:'title',Type:'type'});
+					createInlineForm('Create presentation', content, { Title: 'title', Type: 'type' });
 				}
 			}
 		},
-		pdf : {
-			function : {
-				hint : "Inline form",
-				icon : "inlineForm",
-				execute : () => {
+		pdf: {
+			function: {
+				hint: 'Inline form',
+				icon: 'inlineForm',
+				execute: () => {
 					let content = [
-						{type:'input', placeholder: 'Title'},
-						{type:'input', placeholder: 'Type', value: 'pdf'},
+						{ type: 'input', placeholder: 'Title' },
+						{ type: 'input', placeholder: 'Type', value: 'pdf' }
 					];
-					createInlineForm('Create pdf',content,{Title:'title',Type:'type'});
+					createInlineForm('Create pdf', content, { Title: 'title', Type: 'type' });
 				}
 			}
 		}
 	}
-},
+};
